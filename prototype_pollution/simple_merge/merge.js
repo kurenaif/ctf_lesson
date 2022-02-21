4d065d5b87712c637e7b15e58212f6835d931ed4
kurenaif <antyobido@gmail.com>
2022-02-22 02:07:36 +0900

    add simple case

--- prototype_pollution/simple_merge/merge.js
+++ prototype_pollution/simple_merge/merge.js
@@ -1,7 +1,11 @@
 function isObject(obj) { 
-	return obj !== null && typeof obj === 'object';
+	return obj !== null && typeof obj === 'object'; 
+	// ↓を有効化すると constructor.prototypeが動くようになる
+	// return obj !== null && (typeof obj === 'object' || typeof obj === 'function');
 }
 
+
+// 確実とは言い切れないけど多分 __proto__ の除去で改善可能
 function merge(a, b) {
 	for (let key in b) {
 		if (isObject(a[key]) && isObject(b[key])) { 
@@ -13,4 +17,8 @@ function merge(a, b) {
 	return a; 
 }
 
-// 似たケース: jQueryのextend
+merge({}, JSON.parse('{"__proto__":{"polluted": 1}}'))
+console.log({}.polluted)
+
+merge({}, JSON.parse('{"constructor":{"prototype":{"polluted2": 1}}}'))
+console.log({}.polluted2)
