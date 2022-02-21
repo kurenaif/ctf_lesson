27d5b4dd0c475610e93315163d0ac7497a177fee
kurenaif <antyobido@gmail.com>
2022-02-22 03:18:23 +0900

    master

--- prototype_pollution/simple_merge/merge_fix.js
+++ prototype_pollution/simple_merge/merge_fix.js
@@ -10,6 +10,12 @@ function isObject(obj) {
 // 確実とは言い切れないけど多分 __proto__ の除去で改善可能 (functionが入るとconstructor.prototypeに対して脆弱)
 function merge(a, b) {
 	for (let key in b) {
+		// 修正例2(不完全だけど今回は守れる)
+		/*
+		if(key === '__proto__') {
+			continue;
+		}
+		*/
 		if (isObject(a[key]) && isObject(b[key])) { 
 			merge(a[key], b[key]); 
 		} else { 
@@ -19,8 +25,29 @@ function merge(a, b) {
 	return a; 
 }
 
+// Object.freeze(Object.prototype) // 修正例1
+
 merge({}, JSON.parse('{"__proto__":{"polluted": 1}}'))
 console.log({}.polluted)
 
-merge({}, JSON.parse('{"constructor":{"prototype":{"polluted2": 1}}}'))
+merge({}, JSON.parse('{"constructor":{"prototype":{"polluted2": 2}}}'))
 console.log({}.polluted2)
+
+// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/create 何もないオブジェクトだよ
+merge(Object.create(null), JSON.parse('{"__proto__":{"polluted3": 3}}'))
+console.log({}.polluted3)
+
+// でもObject.createは…
+let nullObject = Object.create(null)
+let normalObject = {}
+nullObject.kurenaif = 12345
+normalObject.kurenaif = 12345
+// nullObject.toString() // ここで例外
+normalObject.toString()
+
+let a = merge({}, JSON.parse('{"a": "a"}'))
+console.log(a.a)
+
+// ロジックによってはこういうのものありだね(mass assignment対策的にやるなら)
+console.log(normalObject.hasOwnProperty('kurenaif')) // => true
+console.log(normalObject.hasOwnProperty('__proto__')) // => false
