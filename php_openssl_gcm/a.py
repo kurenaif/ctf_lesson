63b6af7162e8e7e66fd7e53a6aaa33283ba9c052
kurenaif <antyobido@gmail.com>
2022-04-19 02:19:34 +0900

    wip

---
+++ php_openssl_gcm/a.py
@@ -0,0 +1,26 @@
+import requests
+import base64
+from urllib.parse import urlparse, parse_qs, urlencode
+r = requests.get("http://localhost:8000/encrypt.php")
+r = parse_qs(urlparse(r.text).query)
+ciphertext = r['ciphertext'][0]
+tag = r['tag'][0]
+iv = r['iv'][0]
+print(ciphertext, tag, iv)
+
+ciphertext = base64.urlsafe_b64decode(ciphertext + '=' * (-len(ciphertext) % 4))
+tag = base64.urlsafe_b64decode(tag + '=' * (-len(tag) % 4))
+iv = base64.urlsafe_b64decode(iv+ '=' * (-len(iv) % 4))
+
+ciphertext = base64.urlsafe_b64encode(ciphertext)
+tag = base64.urlsafe_b64encode(tag)
+iv = base64.urlsafe_b64encode(iv)
+
+query = urlencode({
+    'ciphertext': ciphertext,
+    'tag': tag,
+    'iv': iv
+})
+
+r = requests.get("http://localhost:8000/decrypt.php?" + query)
+print(r.text)
