a80a5c8eb397fbb1983f38ba7b134436c1a4f640
kurenaif <antyobido@gmail.com>
2022-11-06 22:03:38 +0900

    rename

--- rand2/java_lcg/brute_force.py
+++ rand2/java_lcg/brute_force.py
@@ -1,6 +1,11 @@
 import random
 # https://crypto.stackexchange.com/questions/37836/problem-with-lll-reduction-on-truncated-lcg-schemes
 
+multiplier = 0x5DEECE66D
+addend = 0xB
+mask = (1 << 48) - 1
+BIT_LEN = 48
+
 class LCG:
     multiplier = 0x5DEECE66D
     addend = 0xB
@@ -9,16 +14,39 @@ class LCG:
 
     def __init__(self, seed):
         self.seed = (seed ^ self.multiplier) & self.mask
+    
+    def set_seed(self, seed):
+        self.seed = seed
 
     def next(self, bits):
         self.seed = (self.seed * self.multiplier + self.addend) & self.mask
         return self.seed >> (48-bits)
 
+def predict_seed(nums, bits):
+    assert len(nums) >= 2
+    
+    prv = nums[0]
+    nxt = nums[1]
+
+    for masked in range(1 << (48-bits)):
+        temp = (prv << (48-bits)) + masked
+        if nxt == ((temp * multiplier + addend) & mask) >> (48 - bits):
+            return temp
+
 lcg = LCG(0)
 nums = []
 
-N = 4
+N = 10
 for i in range(N):
     nums.append(lcg.next(32))
 
-print(nums)
+lcg_predict = LCG(0)
+lcg_predict.set_seed(predict_seed(nums[:2], 32))
+
+predicts = []
+N = 10
+for i in range(N):
+    print(predicts.append(lcg_predict.next(32)))
+
+print(nums[1:])
+print(predicts)
